9e92371e6c9d0d93ab7612794f8791ca3f907e5c
Vijay Karl <67097542+LisherFrost@users.noreply.github.com>
2022-05-07 21:24:28 +0800

    sales report page

--- resources/views/sales_history.blade.php
+++ resources/views/sales_history.blade.php
@@ -10,6 +10,32 @@
     <link rel="stylesheet" href={{ URL::asset('css/sales_history.css'); }} >
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> <!-- google fonts -->
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'> <!-- boxicons -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> <!-- chart -->
+    <!-- pie chart -->
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
 </head>
 <body>
     <!-- navbar -->
@@ -67,6 +93,112 @@
 
 
     <!-- sales history -->
+    <center>
 
+    <h1 class = "nav-labels-text" style = "font-size: 3em;">Sales History</h1>
+    <br><br><br>
+
+    <!-- chart -->
+    <div class="container px-4">
+      <div class="row gx-5">
+        <div class="col">
+         <div class="p-3 border bg-light">
+          <div id="chart_div" style = "width: 100%;"></div> <!-- bar chart -->
+         </div>
+        </div>
+        <div class="col">
+          <div class="p-3 border bg-light">
+            <div id="piechart" style="width: 100%; height: 200px;"></div> <!-- pie chart -->
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br><br>
+
+
+
+    <table class="table table-striped" style = "width: 60%;">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Category</th>
+          <th scope="col">Prod. Name</th>
+          <th scope="col">Seller</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mobile Devices</td>
+          <td>Samsung 69</td>
+          <td>SAMSUNG</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Keyboards / Mouse</td>
+          <td>Razer mouse</td>
+          <td>Razer Inc.</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Instruments</td>
+          <td>YAMAHA Guitar</td>
+          <td>Yamaha</td>
+        </tr>
+      </tbody>
+    </table>
+    </center>
+
+
+    <!-- JAVASCRIPT -->
+
+    <!-- bar chart -->
+    <script>
+      google.charts.load('current', {packages: ['corechart', 'bar']});
+      google.charts.setOnLoadCallback(drawBasic);
+
+      function drawBasic() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('timeofday', 'Time of Day');
+      data.addColumn('number', 'Motivation Level');
+
+      data.addRows([
+        [{v: [8, 0, 0], f: '8 am'}, 1],
+        [{v: [9, 0, 0], f: '9 am'}, 2],
+        [{v: [10, 0, 0], f:'10 am'}, 3],
+        [{v: [11, 0, 0], f: '11 am'}, 4],
+        [{v: [12, 0, 0], f: '12 pm'}, 5],
+        [{v: [13, 0, 0], f: '1 pm'}, 6],
+        [{v: [14, 0, 0], f: '2 pm'}, 7],
+        [{v: [15, 0, 0], f: '3 pm'}, 8],
+        [{v: [16, 0, 0], f: '4 pm'}, 9],
+        [{v: [17, 0, 0], f: '5 pm'}, 10],
+      ]);
+
+      var options = {
+        title: 'Motivation Level Throughout the Day',
+        hAxis: {
+          title: 'Time of Day',
+          format: 'h:mm a',
+          viewWindow: {
+            min: [7, 30, 0],
+            max: [17, 30, 0]
+          }
+        },
+        vAxis: {
+          title: 'Rating (scale of 1-10)'
+        }
+      };
+
+      var chart = new google.visualization.ColumnChart(
+        document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
+    </script>
+
+  
 </body>
 </html>
